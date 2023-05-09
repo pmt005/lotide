c268c6809f055d0141a0b7eddc7addae9da40f0a
Patrick Tumu <patricktumu@gmail.com>
2023-05-08 20:04:55 -0600

    added eqArrays.js updated to modulate

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const emoji = '\uD83D\uDE0D';
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return console.log(`${emoji} Assertion Passed: [${actual}] === [${expected}]`);
-  }
-  return console.log(`Assertion Failed: [${actual}] !== [${expected}]`);
-};
 
 const eqArrays = function(arr1, arr2) {
   let compare1 = arr1.join();
@@ -16,18 +8,4 @@ const eqArrays = function(arr1, arr2) {
   return false;
 };
 
-const testArr1 = ["new","boot","goofin"];
-const testArr2 = ["not","the","same"];
-const emptyArr = [];
-const copyArr = ["new","boot","goofin"];
-
-console.log("This assert should pass");
-assertEqual(eqArrays(testArr1,testArr2), false);
-console.log("This next one should fail");
-assertEqual(eqArrays(testArr1,testArr2), true);
-console.log("This assert should pass");
-assertEqual(eqArrays(testArr1,emptyArr), false);
-console.log("This next one should fail");
-assertEqual(eqArrays(testArr1,emptyArr), true);
-console.log("This assert should pass");
-assertEqual(eqArrays(testArr1,copyArr), true);
+module.exports = eqArrays;
