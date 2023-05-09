39c4d8b47ca10314a74e4aafe7b4faaac6f88a66
Patrick Tumu <patricktumu@gmail.com>
2023-05-09 10:24:53 -0600

    applied new ES6 syntax to export object of index.js

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const eqArrays = require('./eqArrays');
 
 
 module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle,
-  assertArraysEqual: assertArraysEqual,
-  assertEqual: assertEqual,
-  eqArrays: eqArrays
+  head,
+  tail,
+  middle,
+  assertArraysEqual,
+  assertEqual,
+  eqArrays
 };
