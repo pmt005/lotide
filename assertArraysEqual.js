d35d829a74d6ad56292d2026530b7a03f4205a67
Patrick Tumu <patricktumu@gmail.com>
2023-05-08 20:27:37 -0600

    update assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,11 +1,4 @@
-const eqArrays = function(arr1, arr2) {
-  let compare1 = arr1.join();
-  let compare2 = arr2.join();
-  if (compare1 === compare2) {
-    return true;
-  }
-  return false;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(arr1, arr2) {
   const emoji = '\uD83D\uDE0D';
@@ -17,14 +10,4 @@ const assertArraysEqual = function(arr1, arr2) {
   return;
 };
 
-const testArr1 = ["new","boot","goofin"];
-const testArr2 = ["not","the","same"];
-const emptyArr = [];
-const copyArr = ["new","boot","goofin"];
-
-console.log("This assert should fail");
-assertArraysEqual(testArr1,testArr2);
-console.log("This assert should pass");
-assertArraysEqual(testArr1,copyArr);
-console.log("This assert should fail");
-assertArraysEqual(testArr1,emptyArr);
+module.exports = assertArraysEqual;
