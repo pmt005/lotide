71ba36bcb12b218864a9634f7ebf174d2bf432df
Patrick Tumu <patricktumu@gmail.com>
2023-05-08 19:52:28 -0600

    updated assertEqual and head

--- assertEqual.js
+++ assertEqual.js
@@ -1,15 +1,10 @@
 // FUNCTION IMPLEMENTATION
-const emoji = '\uD83D\uDE0D';
-
 const assertEqual = function(actual, expected) {
+  const emoji = '\uD83D\uDE0D';
   if (actual === expected) {
-    return console.log(emoji + 'Assertion Passed: [' + actual + "] === [" + expected + "]");
+    return console.log(`${emoji} Assertion Passed: [${actual}] === [${expected}]`);
   }
-  return console.log("Assertion Failed: [" + actual + "] !== [" + expected + "]");
+  return console.log(`Assertion Failed: [${actual}] !== [${expected}]`);
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual("Lighthouse Labs", "Lighthouse Labs");
-assertEqual(1, 1);
-assertEqual(1,5);
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,12 +1,4 @@
-// FUNCTION copy from previous excercise
-const emoji = '\uD83D\uDE0D';
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return console.log(`${emoji} Assertion Passed: [${actual}] === [${expected}]`);
-  }
-  return console.log(`Assertion Failed: [${actual}] !== [${expected}]`);
-};
+const assertEqual = require('./assertEqual');
 
 const head = function(argArr) {
   if (argArr.length === 0) {
@@ -15,8 +7,5 @@ const head = function(argArr) {
   return argArr[0];
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head([5,6,7]), 9);
-assertEqual(head([]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Goodbye");
+
+module.exports = head;
